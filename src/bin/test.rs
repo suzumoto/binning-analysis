c65302a0c7e8b91010587f67aa20c2c1de72db33
suzumoto <suzuki-motoi643@g.ecc.u-tokyo.ac.jp>
2018-08-31 17:01:15 +0900

    delete jackknife -> binning

---
+++ src/bin/test.rs
@@ -0,0 +1,39 @@
+extern crate binning_analysis;
+extern crate rand;
+extern crate mersenne_twister;
+
+use mersenne_twister::MersenneTwister;
+use rand::{Rng,SeedableRng};
+use binning_analysis::Accumulator;
+
+fn main() {
+    let seed: u64 = 30;
+    let mut rng: MersenneTwister = SeedableRng::from_seed(seed);
+    let mut data = Accumulator::<f64>::new();
+    let mut vec = Vec::<f64>::new();
+    let mut x: f64 = 0.0;
+    for _ in 0 .. 1000000 {
+        let next = x + rng.next_f64() - 0.5;
+        if x.abs() <  next.abs() {
+            let probability = ((x*x - next*next)*0.0125).exp();
+            let random = rng.next_f64();
+            if probability > random {
+                x = next;
+            }
+        } else {
+            x = next;
+        }
+        data.push(x);
+        vec.push(x);
+    }
+    let mean = vec.iter().fold(0.0,|sum, value| sum + value) / vec.len() as f64;
+    let vari = vec.iter().fold(0.0,|sum, value| sum + (value - mean)*(value - mean)) / (vec.len() - 1) as f64;
+    let vari_for_mean = vari / vec.len() as f64;
+    println!("mean:              {}", mean);
+    println!("vari:              {}", vari);
+    println!("variance for mean: {}", vari_for_mean);
+    println!("error for main:    {}", vari_for_mean.sqrt());
+    let result = data.binning();
+    println!("jackknife:         {:?}", result);
+}
+
---
+++ tests/lib.rs
@@ -0,0 +1,37 @@
+extern crate binning_analysis;
+extern crate rand;
+extern crate mersenne_twister;
+
+use mersenne_twister::MersenneTwister;
+use rand::{Rng,SeedableRng};
+use binning_analysis::Accumulator;
+
+#[test]
+fn random_test() {
+    let seed: u64 = 22919;
+    let mut rng: MersenneTwister = SeedableRng::from_seed(seed);
+    let mut data = Accumulator::<f64>::new();
+    let mut x: f64 = 0.0;
+    for _ in 0 .. 65536 * 128 {
+        let next = x + rng.next_f64() - 0.5;
+        if x.abs() <  next.abs() {
+            let probability = ((x*x - next*next)*0.125).exp();
+            let random = rng.next_f64();
+            if probability > random {
+                x = next;
+            }
+        } else {
+            x = next;
+        }
+        data.push(x);
+    }
+    let result = data.binning();
+    let res = binning_analysis::Analyzed::<f64> {
+        mean: 0.0,
+        error: 0.0,
+        correlation_time: 1.0,
+        converged: false,
+        number_of_inputs: 1000,
+    };
+    assert_eq!(result.unwrap(),res);
+}
